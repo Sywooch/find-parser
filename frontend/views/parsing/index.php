bf16d0d204e49c13a920e449cb6d536dce6fbccd
dimka <dimkazinenko18@gmail.com>
2015-11-19 22:09:57 +0200

    вырезал из данных парсинга весь мусор, начал работать с готовыми данными, в бд пока не сохранял

--- frontend/views/parsing/index.php
+++ frontend/views/parsing/index.php
@@ -2,10 +2,24 @@
 $xml = file_get_contents('http://zaporozhe.zap.olx.ua/elektronika/telefony/mobilnye-telefony/q-iphone-5s/rss/');
 $movies = new SimpleXMLElement($xml, LIBXML_NOCDATA);
 foreach($movies->channel->item as $item){
-    $str = $item->description;
-//if(strstr($str, 'Этажность дома: 2') and strstr($str, 'Этаж: 2')){
-    preg_match('#<a.*?>(.*)</a>#is', $str, $m);
-    echo $str . '<br> <hr>';
+//if(strstr($str, 'Цена: 6 500')){
+    preg_match("/Цена:\s\d(.*?)\sгрн/", $item->description, $m);
+
+//    закоментировал, потому что работает пока коряво, нужно доработать регулярку, где из $m[0] только число выталкивается
+//    завтра утром буду с этим разбираться
+//    if(isset($m[0])){
+//        preg_match("/\d/", $m[0], $res);
+//    }
+//    if(isset($res[0])) {
+//        echo 'Заголовок: ' . $item->title . '<br>';
+//        echo '' . str_replace(" ", "", $res[0]) . '<br>';
+//        echo 'Ссылка: ' . $item->link . '<br> <hr>';
+//    }
 //}
+
+    if(isset($m[0])) {
+        echo 'Заголовок: ' . $item->title . '<br>';
+        echo $m[0] . '<br>';
+        echo 'Ссылка: ' . $item->link . '<br> <hr>';
+    }
 }
-?>
