bb88b304d29f593e720931e8a48c02f4d170828d
dimka <dimkazinenko18@gmail.com>
2015-11-19 22:40:17 +0200

    все же распарсил дату :)

--- frontend/views/parsing/index.php
+++ frontend/views/parsing/index.php
@@ -2,24 +2,19 @@
 $xml = file_get_contents('http://zaporozhe.zap.olx.ua/elektronika/telefony/mobilnye-telefony/q-iphone-5s/rss/');
 $movies = new SimpleXMLElement($xml, LIBXML_NOCDATA);
 foreach($movies->channel->item as $item){
-//if(strstr($str, 'Цена: 6 500')){
+//if(strstr($str, 'Цена: 6 500'))
     preg_match("/Цена:\s\d(.*?)\sгрн/", $item->description, $m);
+    if(isset($m[0])){
+        preg_match_all("/\d/", str_replace(" ", "", $m[0]), $prices);
+        foreach($prices as $price) {
+//            $data['title'] = $item->title;
+//            $data['price'] = implode('', $price);
+//            $data['link'] = $item->link;
+            echo 'Заголовок: ' . $item->title . '<br>';
+            echo 'Цена: ' . implode('', $price) . '<br>';
+            echo 'Ссылка: ' . $item->link . '<br> <hr>';
 
-//    закоментировал, потому что работает пока коряво, нужно доработать регулярку, где из $m[0] только число выталкивается
-//    завтра утром буду с этим разбираться
-//    if(isset($m[0])){
-//        preg_match("/\d/", $m[0], $res);
-//    }
-//    if(isset($res[0])) {
-//        echo 'Заголовок: ' . $item->title . '<br>';
-//        echo '' . str_replace(" ", "", $res[0]) . '<br>';
-//        echo 'Ссылка: ' . $item->link . '<br> <hr>';
-//    }
-//}
-
-    if(isset($m[0])) {
-        echo 'Заголовок: ' . $item->title . '<br>';
-        echo $m[0] . '<br>';
-        echo 'Ссылка: ' . $item->link . '<br> <hr>';
+        }
+//        print_r($data);
     }
 }
